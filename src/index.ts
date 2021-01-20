abe1819f608ee49e7cc1f65beb3a4b92f685e287
Constantinos Demetriadis <cdemetriadis@gmail.com>
2021-01-20 20:37:58 +0200

    Added Symbol Layer Names check

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { AssistantPackage } from '@sketch-hq/sketch-assistant-types'
 import { artboardNames } from './rules/artboard-names'
 import { localStyles } from './rules/local-styles'
 import { duplicateArtboards } from './rules/duplicate-artboards'
+import { symbolLayerNames } from './rules/symbol-layer-names'
+import { shapeLayerNames } from './rules/shape-layer-names'
+import { textLayerNames } from './rules/text-layer-names'
 
 const assistant: AssistantPackage = async () => {
   return {
@@ -11,12 +14,18 @@ const assistant: AssistantPackage = async () => {
       artboardNames,
       localStyles,
       duplicateArtboards,
+      symbolLayerNames,
+      shapeLayerNames,
+      textLayerNames,
     ],
     config: {
       rules: {
         'nds-sketch-layout-assistant/artboard-names': { active: true },
         'nds-sketch-layout-assistant/local-styles': { active: true },
         'nds-sketch-layout-assistant/duplicate-artboards': { active: true },
+        'nds-sketch-layout-assistant/symbol-layer-names': { active: true },
+        'nds-sketch-layout-assistant/shape-layer-names': { active: true },
+        'nds-sketch-layout-assistant/text-layer-names': { active: true },
       },
     },
   }
---
+++ src/rules/symbol-layer-names/index.ts
@@ -0,0 +1,26 @@
+import { RuleDefinition } from '@sketch-hq/sketch-assistant-types'
+
+export const symbolLayerNames: RuleDefinition = {
+  rule: async (context) => {
+
+    const sharedSymbols: Map<string, string> = new Map()
+
+    for (const symbolMaster of context.utils.foreignObjects.symbolMaster) {
+        sharedSymbols.set(symbolMaster.symbolID, symbolMaster.name)
+    }
+
+    for (const layer of context.utils.objects.anyLayer) {
+      if (layer._class != 'symbolInstance') continue
+
+      const symbolName = sharedSymbols.get(layer.symbolID)
+
+      if (symbolName != undefined && layer.name == symbolName) continue
+      context.utils.report(layer.name + " - " + symbolName, layer)
+
+    }
+
+  },
+  name: 'nds-sketch-layout-assistant/symbol-layer-names',
+  title: 'Symbol Layer Names',
+  description: 'Reports symbol layer names that that do not match their symbol master names.',
+}
